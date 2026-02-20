7c6b175ed7713ea4971da94210190448d858c315
Joshua Stan <lego24451@gmail.com>
2026-02-20 09:24:37 -0600

    game avance 1.0

--- src/labexamen1/Game.java
+++ src/labexamen1/Game.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package labexamen1;
 
-/**
- *
- * @author spodi
- */
-public class Game {
+import java.util.ArrayList;
+import java.util.Calendar; 
+import java.util.Scanner; 
+
+public class Game extends  RentItem {
+    
+    private Calendar fechaPublicacion; 
+    private ArrayList<String> especificaciones; 
+    private String imagen;
+    
+    public Game(String codigoUnico,String nombreItem, double precioRenta, Calendar fechaPulibcacion, ArrayList<String> especificaciones, String imagen){
+    
+        super(codigoUnico, nombreItem, 20); 
+        this.fechaPublicacion = Calendar.getInstance(); 
+        this.especificaciones = new ArrayList<String>(); 
+        this.imagen = imagen; 
+        
+   }
+    public double getPrecioRenta(){
+        return 20.0; 
+    }
+    public void setFechaPublicacion(int year, int mes, int dia){
+        
+    }
+     
+    public ArrayList<String> listEspecificaciones(){
+        return especificaciones; 
+    }
+   
     
 }
