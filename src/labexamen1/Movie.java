9c08b5f8fd9a21971f9aac18576d814372604055
Andrés Díaz <andresalbertodiazrevelo@gmail.com>
2026-02-20 09:38:59 -0600

    Estructura de Movie

--- src/labexamen1/Movie.java
+++ src/labexamen1/Movie.java
@@ -3,11 +3,64 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package labexamen1;
-
+import java.util.Calendar;
 /**
  *
  * @author user
  */
-public class Movie {
+public class Movie extends RentItem {
+    
+    private Calendar fechaEstreno;
+    public String Categoria = " ";
+    
+    
+     Movie(String codigoUnico,String nombreItem, double precioRenta){
+     super (codigoUnico,nombreItem, precioRenta);
+    this.fechaEstreno = Calendar.getInstance();
+}
+
+    public Calendar getFechaEstreno() {
+        return fechaEstreno;
+    }
+
+    public void setFechaEstreno(Calendar fechaEstreno) {
+        this.fechaEstreno = fechaEstreno;
+    }
+     
+    
+    @Override
+    public String toString(){
+    return super.toString() + "\nEstado: "+getEstado() + "\n Movie";
+            
+    }
+    
+    public String getEstado(){
+        Calendar hoy = Calendar.getInstance();
+        Calendar limite = (Calendar) fechaEstreno.clone();
+        limite.add(Calendar.MONTH, 3);
+        
+        if (hoy.before(limite)){
+            Categoria = "ESTRENO";
+        }
+        else {
+            Categoria = "Normal";
+        }
+        return Categoria;
+    }
+    
+    
+    
+    
+    
+     
+    public double pagoRenta(int dias){
+        int precioRenta = 0;
+        
+        
+        return precioRenta;
+    }
+    
+    
+    
     
 }
